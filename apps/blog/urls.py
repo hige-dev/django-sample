cb6717a5fddc4431e779173b1d5b72c7b4b86e5a
miyama <yasuhiro.miyama@rakus.co.jp>
2022-07-13 08:44:02 +0900

    export csv

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -1,5 +1,5 @@
 from rest_framework import routers
-from .views import UserViewSet, EntryViewSet, get_title
+from .views import UserViewSet, EntryViewSet, get_title, export_entries
 from django.urls import path
 
 router = routers.DefaultRouter()
@@ -8,4 +8,5 @@ router.register(r'entries', EntryViewSet)
 
 urlpatterns = [
     path(r'get_title', get_title),
+    path(r'export_entries', export_entries)
 ]
